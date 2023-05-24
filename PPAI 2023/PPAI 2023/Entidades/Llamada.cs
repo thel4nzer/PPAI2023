cfc9f99a8ca5bac496cc54b47895568388d5f5c5
Maier Sananez Deborah <debomaier@mi.unc.edu.ar>
2023-05-24 15:45:04 -0300

    cambio clase llamada

--- PPAI 2023/PPAI 2023/Entidades/Llamada.cs
+++ PPAI 2023/PPAI 2023/Entidades/Llamada.cs
@@ -18,48 +18,29 @@ namespace PPAI_2023.Entidades
         private Cliente cliente;
         private List<RespuestaCliente> respuestaCliente = new List<RespuestaCliente>();
         private string duracion;
-
-
-
-
-        public Llamada(string descOperador, string detalle, string duracion, bool encuestaEnv, string observacion, Cliente cliente)
-        {
-            this.descripcionOperador = descOperador;
-            this.detalleAccionRequerida = detalle;
-            this.duracion = duracion
-            this.encuestaEnviada = encuestaEnv;
-            this.observacionAuditor = observacion;
-            this.cliente = cliente;
-        }
         
-        public string DescOperador
-        {
-            get { return descripcionOperador; }
-            set { descripcionOperador = value; }
-        }
+        public string DescOperador { get => descripcionOperador; set => descripcionOperador = value; }
+        public string Duracion { get => duracion; set => duracion = value; }
+        public string Detalle { get => detalleAccionRequerida;set => detalleAccionRequerida = value; }
+        
+        public bool EncuestaEnv { get => encuestaEnviada; set => encuestaEnviada = value; }
 
-        public string Duracion
-        {
-            get { return duracion; }
-            set => duracion = value;
-        }
+        public string Observacion { get => observacionAuditor; set => observacionAuditor = value; } 
 
-        public string Detalle
-        {
-            get => detalleAccionRequerida;
-            set => detalleAccionRequerida = value;
-        }
+        public Cliente Cliente { get => cliente; set => cliente = value; }
+        public List<RespuestaCliente> RespuestaCliente { get => respuestaCliente; set => respuestaCliente = value; }
+        public List<CambioEstado> CambioEstado { get => cambioEstado; set => cambioEstado = value; }
 
-        public bool EncuestaEnv
+        public Llamada(string descripcionOperador, string detalleAccionRequerida, bool encuestaEnviada, string observacionAuditor, List<CambioEstado> cambioEstado, Cliente cliente, List<RespuestaCliente> respuestaCliente, string duracion, string descOperador)
         {
-            get => encuestaEnviada;
-            set => encuestaEnviada = value;
-        }
-
-        public string Observacion
-        {
-            get => observacionAuditor;
-            set => observacionAuditor = value;
+            this.descripcionOperador = descripcionOperador;
+            this.detalleAccionRequerida = detalleAccionRequerida;
+            this.encuestaEnviada = encuestaEnviada;
+            this.observacionAuditor = observacionAuditor;
+            this.respuestaCliente = respuestaCliente;
+            this.duracion = duracion;
+            this.cambioEstado = cambioEstado;
+            this.cliente = cliente;
         }
 
         public bool esDePeriodo(DateTime fechaInicio, DateTime fechaFin)
